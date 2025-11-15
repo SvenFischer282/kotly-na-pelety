4b30d8649d3c9ce3a10d3201c12a865649c79792
SvenFischer282 <81426840+SvenFischer282@users.noreply.github.com>
2025-11-15 21:54:24 +0100

    updt

--- src/hooks/useGoogleAnalytics.ts
+++ src/hooks/useGoogleAnalytics.ts
@@ -1,7 +1,7 @@
-import { useEffect } from 'react';
-import { CookiePreferences } from './useCookieConsent';
+import { useEffect } from "react";
+import { CookiePreferences } from "./useCookieConsent";
 
-const GA_MEASUREMENT_ID = 'G-51W617QD7D';
+const GA_MEASUREMENT_ID = "G-51W617QD7D";
 
 export const useGoogleAnalytics = (preferences: CookiePreferences) => {
   useEffect(() => {
@@ -16,19 +16,18 @@ export const useGoogleAnalytics = (preferences: CookiePreferences) => {
     }
 
     // Load GA script
-    const script = document.createElement('script');
+    const script = document.createElement("script");
     script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`;
     script.async = true;
     document.head.appendChild(script);
 
     // Initialize gtag
     window.dataLayer = window.dataLayer || [];
-    window.gtag = function() {
+    window.gtag = function () {
       window.dataLayer.push(arguments);
     };
-    window.gtag('js', new Date());
-    window.gtag('config', GA_MEASUREMENT_ID);
-
+    window.gtag("js", new Date());
+    window.gtag("config", GA_MEASUREMENT_ID);
   }, [preferences.analytics]);
 };
 
